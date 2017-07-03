96b6b50741ae0b33a721746b78fc0b969da09113
David Rodrigues <david.proweb@gmail.com>
2017-07-03 13:58:33 -0300

    Sentry: don't send user context;

--- config/sentry.php
+++ config/sentry.php
@@ -5,5 +5,5 @@ declare(strict_types = 1);
 return [
     'dsn'                      => env('SENTRY_DSN'),
     'breadcrumbs.sql_bindings' => true,
-    'user_context'             => true,
+    'user_context'             => false,
 ];
