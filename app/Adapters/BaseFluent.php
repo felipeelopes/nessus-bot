a8e1b225d1d6e3bde353913aa398814e6b4903f8
David Rodrigues <david.proweb@gmail.com>
2017-08-31 14:19:51 -0300

    BaseFluent: method to avoid recursivity;

--- app/Adapters/BaseFluent.php
+++ app/Adapters/BaseFluent.php
@@ -6,6 +6,7 @@ namespace Application\Adapters;
 
 use Illuminate\Support\Collection;
 use Illuminate\Support\Fluent;
+use JsonSerializable;
 
 /**
  * @property-read BaseFluent $parent Parent instance.
@@ -24,6 +25,40 @@ class BaseFluent extends Fluent
     }
 
     /**
+     * Strip recursivity from array.
+     * @param array $attributes Attributes to be analyzed.
+     * @param array $recursions Recursions to be avoided.
+     */
+    private static function stripRecursivity(array $attributes, array &$recursions)
+    {
+        foreach ($attributes as &$attribute) {
+            if (is_array($attribute)) {
+                $attribute = self::stripRecursivity($attribute, $recursions);
+            }
+
+            if (!is_object($attribute)) {
+                continue;
+            }
+
+            if (in_array($attribute, $recursions, true)) {
+                $attribute = '{&...}';
+                continue;
+            }
+
+            $recursions[] = $attribute;
+
+            if ($attribute instanceof JsonSerializable) {
+                $attribute = self::stripRecursivity($attribute->jsonSerialize(), $recursions);
+                continue;
+            }
+
+            $attribute = (array) $attribute;
+        }
+
+        return $attributes;
+    }
+
+    /**
      * Process Fluent data if it is declared and instantiate a new class with this.
      * @param string $key   Data key.
      * @param string $class Data class type.
@@ -36,6 +71,17 @@ class BaseFluent extends Fluent
 
             $this->offsetSet($key, new $class($value));
         }
+    }
+
+    /**
+     * Convert the Fluent instance to an array.
+     * It should remove any recursion.
+     */
+    public function toArrayUnrecursive(): array
+    {
+        $recursions = [ $this ];
+
+        return self::stripRecursivity($this->attributes, $recursions);
     }
 
     /**
